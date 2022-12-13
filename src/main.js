be680247c29164350129520718129f8858029cec
changeonPark <qkrcksrjs@gmail.com>
2022-12-13 16:30:20 +0900

    middleware basic

--- src/main.js
+++ src/main.js
@@ -1,12 +1,30 @@
 // @ts-check
 
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
 
-app.use("/", (req, res) => {
-  res.send("Hello, Express");
+app.use("/", async (req, res, next) => {
+  const fileContent = await fs.promises.readFile(".gitignore");
+
+  const requestedAt = new Date();
+  //@ts-ignore
+  req.requestedAt = requestedAt;
+  //@ts-ignore
+  req.fileContent = fileContent
+
+  console.log("middleware 1");
+  next();
+});
+
+app.use((req, res) => {
+  //@ts-ignore
+  console.log(req.requestedAt);
+  console.log("middleware 2");
+  //@ts-ignore
+  res.send(`Hello, Express at ${req.requestedAt}\n ${req.fileContent}`);
 });
 
 app.listen(PORT, () => {
